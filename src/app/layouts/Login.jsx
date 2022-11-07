b5bfbdab52a5a6165176b035b383cfdf19b1b880
Lada Penkova <whitethe2001@gmail.com>
2022-11-07 20:55:27 +0200

    page login

--- src/app/layouts/Login.jsx
+++ src/app/layouts/Login.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+
+import LoginForm from "../components/ui/LoginForm";
+import RegisterForm from "../components/ui/RegisterForm";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  const [formType, setFormType] = useState("login");
+
+  const handleChandeFormType = () => {
+    setFormType((prevState) =>
+      prevState === "register" ? "login" : "register"
+    );
+  };
+
+  return (
+    <>
+      <div className="row wrapper-center">
+        <div className="col-4 shadow p-3">
+          {formType === "register" ? (
+            <>
+              <h3 className="headline-form">Register</h3>
+              <RegisterForm />
+              <div className="text-and-link">
+                <p>Already have account?</p>
+                <a
+                  className="btn-link"
+                  role="button"
+                  onClick={handleChandeFormType}
+                >
+                  Sign in
+                </a>
+              </div>
+            </>
+          ) : (
+            <>
+              <h3 className="headline-form">Authorization</h3>
+              <LoginForm />
+              <div className="text-and-link">
+                <p>Dont have account?</p>
+                <a
+                  className="btn-link"
+                  role="button"
+                  onClick={handleChandeFormType}
+                >
+                  Sign up
+                </a>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
