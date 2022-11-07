81d43b7bd4ffa75e89a8b3a828afb3bf00f3c5ee
Lada Penkova <whitethe2001@gmail.com>
2022-11-07 16:38:25 +0200

    Create structure project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './styles/index.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
